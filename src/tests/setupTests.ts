650ce8fdb997f323fd645237f79ded537f07b543
edenaharon1 <edenah9@gmail.com>
2025-03-24 14:17:16 +0200

    middleware files

---
+++ src/tests/setupTests.ts
@@ -0,0 +1,37 @@
+import request from "supertest";
+import initApp from "../server";
+import mongoose from "mongoose";
+import { Express } from "express";
+import { expect, beforeAll, afterAll } from '@jest/globals'; // ייבוא נכון
+
+let app: Express;
+export let testUser: { token: string; _id: string };
+
+beforeAll(async () => {
+    console.log("Global beforeAll");
+    app = await initApp();
+
+    const loginResponse = await request(app)
+        .post("/auth/login")
+        .send({
+            email: "test@user.com",
+            password: "testpassword",
+        });
+
+    expect(loginResponse.statusCode).toBe(200);
+    expect(loginResponse.body.accessToken).toBeDefined();
+
+    testUser = {
+        token: loginResponse.body.accessToken,
+        _id: loginResponse.body._id,
+    };
+
+    console.log("testUser:", testUser);
+});
+
+afterAll(async () => {
+    console.log("Global afterAll");
+    await mongoose.connection.close();
+});
+
+export { app };
