e779ea9938df2ddefda6761ed53416a52bf97b17
edenaharon1 <edenah9@gmail.com>
2025-03-24 14:00:42 +0200

    user controller,model,routes,test

---
+++ src/models/user_model.ts
@@ -0,0 +1,29 @@
+import mongoose from "mongoose";
+
+export interface IUser {
+  username: string;
+  email: string;
+  password: string;
+  _id?: string;
+  refreshToken?: string[];
+}
+
+const userSchema = new mongoose.Schema<IUser>({
+  email: {
+    type: String,
+    required: true,
+    unique: true,
+  },
+  password: {
+    type: String,
+    required: true,
+  },
+  refreshToken: {
+    type: [String],
+    default: [],
+  }
+});
+
+const userModel = mongoose.model<IUser>("Users", userSchema);
+
+export default userModel;
