ec7bad4d1f042fd6aa1405da055168eef39218e9
edenaharon1 <edenah9@gmail.com>
2025-03-24 16:02:19 +0200

    index.css

--- Frontend/src/SignUp.tsx
+++ Frontend/src/SignUp.tsx
@@ -1,6 +1,6 @@
 import React, { useState } from "react";
 import styles from './Login.module.css';
-import logo from './Images/Logo.png';
+import logo from './Images/hobiz_logo-removebg-preview.png';
 import { Link, useNavigate } from 'react-router-dom';
 import axios from 'axios';
 
--- Frontend/src/components/homeComponent.tsx
+++ Frontend/src/components/homeComponent.tsx
@@ -1,8 +1,8 @@
 import React from 'react';
 import { Link } from 'react-router-dom';
 import styles from './Home.module.css';
-import CreatePostModal from '../CreatePostModal';
-import { Post } from '../Api';
+import CreatePostModal from '../CreatePost';
+import { Post } from "../Api"
 
 interface HomeComponentsProps {
     posts: Post[];
